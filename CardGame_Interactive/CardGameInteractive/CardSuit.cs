09a8be19402e30ca4163bcc3e05b7bb941176d7c
Artem Kotliar <kotliar@sheridancollege.ca>
2025-01-31 14:41:41 -0500

    Defined all field variables

---
+++ CardGame_Interactive/CardGameInteractive/CardSuit.cs
@@ -0,0 +1,11 @@
+namespace CardGameInteractive;
+/// <summary>
+/// Defines the cards suits that cards have.
+/// </summary>
+public enum CardSuit
+{
+    Diamonds = 1,
+    Clubs,
+    Hearts,
+    Spades
+}
---
+++ CardGame_Interactive/CardGameInteractive/Score.cs
@@ -0,0 +1,16 @@
+namespace CardGameInteractive;
+/// <summary>
+/// Define the score of the game.
+/// </summary>
+public class Score
+{
+    /// <summary>
+    /// The number of rounds won by the player of the game.
+    /// </summary>
+    private int _playerScore;
+
+    /// <summary>
+    /// The number of rounds won by the house.
+    /// </summary>
+    private int _houseScore;
+}
